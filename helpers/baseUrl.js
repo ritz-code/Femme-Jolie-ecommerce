d7a09c6e8216ebd6d1001ee66ad39aa490f1cdb7
Ritu R <ritu.sarma29@gmail.com>
2021-07-11 21:37:15 -0400

    minor fix4

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "femmejolie.vercel.app" : "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://femmejolie.vercel.app" : "http://localhost:3000"
 
 export default baseUrl
